717beab1731d3e892fbdec1c6749c0e00578e8a5
Andrew Warren <anwarren@vbi.vt.edu>
2014-01-21 21:12:18 -0500

    attempting to replace location numbers with highlight links

--- js/gexfjs.js
+++ js/gexfjs.js
@@ -204,14 +204,14 @@ function replaceLocationLinks(location_ref) {
 
 function displayPath(_eid, _path_str){
 	GexfJS.params.activeEdges={};
-	if(typeof _eid !== undefined){
+	if(typeof _eid !== "undefined"){
 		var _e = GexfJS.graph.edgeLookup[_eid];
 	}
 	else{
 		var _e = undefined;
 	}
 	//var _e = (typeof _eid !== undefined ? GexfJS.graph.edgeLookup[_eid] : undefined);
-	var _pathList = (typeof _e !== undefined ? _e.path : (typeof _path_str !== undefined ? _path_str.split(/[ ,]+/) : undefined));
+	var _pathList = (typeof _e !== "undefined" ? _e.path : (typeof _path_str !== "undefined" ? _path_str.split(/[ ,]+/) : undefined));
 		
 	for (var i in _pathList){
 		var _elist=GexfJS.path_highlights[_pathList[i]];
