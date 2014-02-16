341aafd86f8142550589d2618ea7dc3b4997b4ee
Andrew Warren <anwarren@vbi.vt.edu>
2014-02-16 17:35:14 -0500

    adding code for accordion genome menu

--- js/gexfjs.js
+++ js/gexfjs.js
@@ -224,7 +224,7 @@ function getPATRICLocations(location_ref, locations, location_order){
 				}
 				result.push('<div id="genome_list">\n');
 				for (var g in _genome_menu){
-					result.push('<h3 id="'+g+'_header">'+_genome_menu[g].genome_name+'</h3>\n<div>\n');
+					result.push('<h6 id="'+g+'_header">'+_genome_menu[g].genome_name+'</h6>\n<div>\n');
 					result.push('<ul id = '+g+'_sids >\n');
 					for (var j in sid_list=_genome_menu[g].sids){
 						result.push('<li> <div class="smallbox" style="background: black"></div>'+'<a href="#" onclick="displayPath(undefined,'+"'"+sid_list[j]+"'"+'); return false;">'+_locations[sid_list[j]].description+'</a>: '+_locations[sid_list[j]].base)+' </li>/n';
